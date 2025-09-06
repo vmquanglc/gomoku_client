09560e414f1c978fa52d064a8aedd668969676ee
vmquanglc <vmquanglc@gmail.com>
2025-09-06 21:46:18 +0700

    test socket

--- src/game/index.js
+++ src/game/index.js
@@ -1,1 +1,22 @@
-console.log("game index.js");
+const popupConnectServer = new PopupConnectServer();
+const state = new Proxy(
+  { connectedServer: false },
+  {
+    set(target, key, value) {
+      if (key === "connectedServer") {
+        value ? popupConnectServer.close() : popupConnectServer.show()
+      }
+      target[key] = value;
+      return true;
+    },
+  }
+);
+
+const socket = io("http://localhost:1995");
+socket.on("connect", () => {
+  state.connectedServer = true;
+});
+socket.on("connect_error", (err) => {
+  state.connectedServer = false;
+});
+socket.on("disconnect", (reason) => {});
